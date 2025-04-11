e2a9b71343877ce5507be2576d852a95223c5eda
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-04-12 02:26:49 +0300

    feat: GET route for watermark image generation

---
+++ src/app/route.tsx
@@ -0,0 +1,23 @@
+import { NextRequest } from "next/server";
+import Watermark from "../utilities/Watermark";
+
+export const GET = async (request: NextRequest) => {
+  try {
+    const { searchParams } = request.nextUrl;
+
+    const source = searchParams.get("source");
+    const text = searchParams.get("text");
+
+    if (!source) return new Response("Missing source", { status: 400 });
+
+    if (!text) return new Response("Missing text", { status: 400 });
+
+    const imageResponse = await Watermark.generateImageResponse(source, text);
+    return imageResponse;
+  } catch (error) {
+    return new Response(
+      error instanceof Error ? error.message : "Unknown error",
+      { status: 500 }
+    );
+  }
+};
