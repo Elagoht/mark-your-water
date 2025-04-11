af5b83804ad95205f6a35748712f8e9743169988
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-04-12 02:26:41 +0300

    feat: Watermark utility for image processing and generation

---
+++ src/utilities/Watermark.ts
@@ -0,0 +1,106 @@
+import { ImageResponse } from 'next/og';
+import { createElement } from 'react';
+import sharp from 'sharp';
+
+class Watermark {
+  public static async getImageSize(imageUrl: string) {
+    try {
+      const res = await fetch(imageUrl, {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        }
+      });
+
+      if (!res.ok) {
+        throw new Error(`Failed to fetch image: ${res.statusText}`);
+      }
+
+      const contentType = res.headers.get('content-type');
+
+      if (!contentType || !contentType.startsWith('image/')) {
+        throw new Error('Fetched content is not an image');
+      }
+
+      if (contentType === 'image/webp') {
+        throw new Error('WebP format is not supported');
+      }
+
+      const arrayBuffer = await res.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+      const metadata = await sharp(buffer).metadata();
+
+      if (!metadata.width || !metadata.height) {
+        throw new Error('Image size not found');
+      }
+
+      return { width: metadata.width, height: metadata.height };
+    } catch (error) {
+      throw new Error(`Error getting image size: ${
+        error instanceof Error ? error.message : 'Unknown error'
+      }`);
+    }
+  }
+
+  public static async generateReactComponent(
+    imageUrl: string,
+    size: { width: number, height: number },
+    watermarkText: string,
+  ): Promise<React.ReactElement> {
+    return createElement('div', {
+      style: {
+        width: `${size.width}px`,
+        height: `${size.height}px`,
+        position: 'relative',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+    }, [
+      createElement(
+        'img',
+        {
+          src: imageUrl,
+          style: { width: '100%', height: '100%', objectFit: 'cover' },
+        },
+      ),
+      createElement(
+        'span',
+        {
+          style: {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%) rotate(-15deg)',
+            padding: '0.5rem 1rem',
+            borderRadius: '0.5rem',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            color: 'white',
+            fontSize: '2rem',
+            fontWeight: 'bold',
+            opacity: 0.5,
+          },
+        },
+        watermarkText,
+      ),
+    ]);
+  }
+
+  public static async generateImageResponse(
+    imageUrl: string,
+    watermarkText: string,
+  ) {
+    const size = await this.getImageSize(imageUrl);
+    const component = await this.generateReactComponent(
+      imageUrl,
+      size,
+      watermarkText,
+    );
+
+    return new ImageResponse(component, {
+      width: size.width,
+      height: size.height,
+    });
+  }
+}
+
+export default Watermark;
